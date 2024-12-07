464710d28d5b6efc5eb1d4c2b44316f64a8259e4
Yura Beseda <yuriy.beseda@gmail.com>
2024-12-07 00:29:26 -0500

    Add encryption and decryption utilities; update JWT expiration and message handling

--- src/models/chat/resolvers/chat.Chat.messages.query.ts
+++ src/models/chat/resolvers/chat.Chat.messages.query.ts
@@ -5,36 +5,39 @@ import type { Chat } from "../chat.types";
 import type { TMessage } from "../../message/message.types";
 import type { types } from "cassandra-driver";
 import { isUserAChatMemberMiddleware } from "../service/isUserAChatMemeber";
+import { decrypt } from "../../../utils/cryptoUtils";
 
 export const chatMessages = async (
-    chat: Chat,
-    { lastMessageId = null }: { lastMessageId: types.TimeUuid | null },
-    context: AppQraphQLContext
+	chat: Chat,
+	{ lastMessageId = null }: { lastMessageId: types.TimeUuid | null },
+	context: AppQraphQLContext
 ): Promise<TMessage[]> => {
-    console.log('lastMessageId', lastMessageId);
+	console.log("lastMessageId", lastMessageId);
 
-    const user = await isAuthenticatedMiddleware(context);
-    await isUserAChatMemberMiddleware(user.id, chat.id);
+	const user = await isAuthenticatedMiddleware(context);
+	await isUserAChatMemberMiddleware(user.id, chat.id);
 
-    // Fetch messages with pagination
-    const pageSize = 2;
-    const messagesQuery = lastMessageId ?
-        `SELECT * FROM messages 
+	// Fetch messages with pagination
+	const pageSize = 2;
+	const messagesQuery = lastMessageId
+		? `SELECT * FROM messages 
         WHERE chat_id = ${chat.id.toString()} 
         AND id < ${lastMessageId.toString()} 
-        LIMIT ${pageSize}` :
-        `SELECT * FROM messages 
+        LIMIT ${pageSize}`
+		: `SELECT * FROM messages 
         WHERE chat_id = ${chat.id.toString()} 
         LIMIT ${pageSize}`;
-    const messagesResult = await client.execute(messagesQuery, [], { prepare: true });
+	const messagesResult = await client.execute(messagesQuery, [], {
+		prepare: true,
+	});
 
-    return messagesResult.rows.map(row => ({
-        id: row.id,
-        chat_id: row.chat_id,
-        user_id: row.user_id,
-        type: row.type,
-        content: row.content,
-        status: row.status,
-        timestamp: row.timestamp,
-    }));
+	return messagesResult.rows.map((row) => ({
+		id: row.id,
+		chat_id: row.chat_id,
+		user_id: row.user_id,
+		type: row.type,
+		content: decrypt(row.content),
+		status: row.status,
+		timestamp: row.timestamp,
+	}));
 };
