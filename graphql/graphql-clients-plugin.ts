00d9e0e0b46a6df73bea387aa8db3c9d92235f87
AlexanderBorysenko <borysenko.alexander@gmail.com>
2024-12-20 04:55:37 +0200

    fix: update AuthorizationToken cookie domain and SameSite attribute for localhost support

--- graphql/graphql-clients-plugin.ts
+++ graphql/graphql-clients-plugin.ts
@@ -5,12 +5,13 @@ export default defineNuxtPlugin((nuxtApp) => {
     const config = useRuntimeConfig();
     const apiEndpoint = config.public.severHost;
     const wsEndpoint = config.public.wsHost;
+    const isLocalhost = config.public.severHost.includes('localhost');
 
     const AuthorizationToken = useCookie('Authorization', {
         secure: true,
         httpOnly: false,
-        sameSite: 'none',
-        domain: '.keysharer.com',
+        sameSite: 'lax',
+        domain: isLocalhost ? 'localhost' : '.keysharer.com',
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
         path: "/",
     });
