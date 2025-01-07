610cec6703b2ffb96a2ac642043965c6da714479
Yura Beseda <yuriy.beseda@gmail.com>
2025-01-07 17:40:01 -0500

    Removed query from database initialization file

--- src/db/initialize.ts
+++ src/db/initialize.ts
@@ -1,43 +1,59 @@
-import { types } from 'cassandra-driver';
-import userActiveSessionsService from '../models/user/service/userActiveSessionsService';
-import userDBService from '../models/user/service/userDBService';
-import { Role } from '../models/user/user.types';
-import { client } from './client';
+import { types } from "cassandra-driver";
+import userActiveSessionsService from "../models/user/service/userActiveSessionsService";
+import userDBService from "../models/user/service/userDBService";
+import { Role } from "../models/user/user.types";
 
 export const initializeDatabase = async () => {
-    await userActiveSessionsService.resetAllUsersActiveSessionsCount();
+	await userActiveSessionsService.resetAllUsersActiveSessionsCount();
 
-    const users = [
-        { username: 'Igor', email: 'elon@gmail.com', displayName: 'Igor' },
-        { username: 'Yurii', email: 'elon@gmail.com', displayName: 'Yurii' },
-        { username: 'Alex', email: 'elon@gmail.com', displayName: 'Alex' },
-        { username: 'ElonMusk', email: 'elon@gmail.com', displayName: 'Elon Musk' },
-        { username: 'DonaldTrump', email: 'donald@gmail.com', displayName: 'Donald Trump' },
-        { username: 'BillGates', email: 'bill@gmail.com', displayName: 'Bill Gates' },
-        { username: 'JeffBezos', email: 'jeff@gmail.com', displayName: 'Jeff Bezos' },
-        { username: 'MarkZuckerberg', email: 'mark@gmail.com', displayName: 'Mark Zuckerberg' },
-        { username: 'WarrenBuffet', email: 'warren@gmail.com', displayName: 'Warren Buffet' }
-    ];
+	const users = [
+		{ username: "Igor", email: "elon@gmail.com", displayName: "Igor" },
+		{ username: "Yurii", email: "elon@gmail.com", displayName: "Yurii" },
+		{ username: "Alex", email: "elon@gmail.com", displayName: "Alex" },
+		{
+			username: "ElonMusk",
+			email: "elon@gmail.com",
+			displayName: "Elon Musk",
+		},
+		{
+			username: "DonaldTrump",
+			email: "donald@gmail.com",
+			displayName: "Donald Trump",
+		},
+		{
+			username: "BillGates",
+			email: "bill@gmail.com",
+			displayName: "Bill Gates",
+		},
+		{
+			username: "JeffBezos",
+			email: "jeff@gmail.com",
+			displayName: "Jeff Bezos",
+		},
+		{
+			username: "MarkZuckerberg",
+			email: "mark@gmail.com",
+			displayName: "Mark Zuckerberg",
+		},
+		{
+			username: "WarrenBuffet",
+			email: "warren@gmail.com",
+			displayName: "Warren Buffet",
+		},
+	];
 
-    for (const user of users) {
-        if (!(await userDBService.findUserByUsername(user.username))) {
-            await userDBService.createUser({
-                id: types.Uuid.random(),
-                username: user.username,
-                email: user.email,
-                emailVerified: true,
-                password: 'Password@123',
-                role: Role.USER,
-                avatar: '',
-                displayName: user.displayName
-            });
-        }
-    }
-
-    const query = `
-        ALTER TABLE app_keyspace.message_files
-        ADD original_file_name TEXT
-    `;
-
-    await client.execute(query);
-}
+	for (const user of users) {
+		if (!(await userDBService.findUserByUsername(user.username))) {
+			await userDBService.createUser({
+				id: types.Uuid.random(),
+				username: user.username,
+				email: user.email,
+				emailVerified: true,
+				password: "Password@123",
+				role: Role.USER,
+				avatar: "",
+				displayName: user.displayName,
+			});
+		}
+	}
+};
